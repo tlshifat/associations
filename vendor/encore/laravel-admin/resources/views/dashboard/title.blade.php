0af9a627c865ae99be4436f23efad74d414b2750
Shifat <shifat.it@gmail.com>
2019-11-22 11:14:22 +0600

    message removed

--- vendor/encore/laravel-admin/resources/views/dashboard/title.blade.php
+++ vendor/encore/laravel-admin/resources/views/dashboard/title.blade.php
@@ -28,8 +28,8 @@
 <div class="title">
     Associations
 </div>
-{{--<div class="links">--}}
-{{--    <a href="https://github.com/z-song/laravel-admin" target="_blank">Github</a>--}}
-{{--    <a href="http://laravel-admin.org/docs"  target="_blank">Documentation</a>--}}
-{{--    <a href="http://laravel-admin.org/demo"  target="_blank">Demo</a>--}}
-{{--</div>--}}
+{{--<div class="links">
+    <a href="https://github.com/z-song/laravel-admin" target="_blank">Github</a>
+    <a href="http://laravel-admin.org/docs"  target="_blank">Documentation</a>
+    <a href="http://laravel-admin.org/demo"  target="_blank">Demo</a>
+</div>--}}
